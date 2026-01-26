9886bed44b4fc6da43d99a6ed2c2ccbeefcb5a27
Yarlei Cruz <yarleicruz1998@gmail.com>
2026-01-26 10:49:48 -0300

    Update test description for online web app check

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('web app should be online', async ({ page }) => {
+test('The Velô web app should be online', async ({ page }) => {
   await page.goto('http://localhost:5173');
 
   // Expect a title "to contain" a substring.
