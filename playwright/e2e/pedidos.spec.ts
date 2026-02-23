5d2cef7f9162a2617ffb386da975737fe47264d1
First Last <yarleicruz1998@gmail.com>
2026-02-23 11:57:55 -0300

    feat: enhance order lookup tests with additional scenarios and dynamic test data for improved coverage

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -2,31 +2,33 @@ import { test, expect } from '@playwright/test';
 import { generateOrderNumber } from '../support/helpers'
 // AAA Arrange Act Assert
 test.describe('order lookup', () => {
+  
   test.beforeEach(async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:5173/');
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+    // Arrange
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
   })
+
   test('Should verify an approved order', async ({ page }) => {
     // testData
     const testData = {
       orderCode: 'VLO-U9BW56',
       orderStatus: 'APROVADO',
     };
-  
+
     // Act
     await page.getByLabel('Número do Pedido').fill(testData.orderCode);
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
-  
+
     // Assert
     // Locate the paragraph with the text "Pedido", go up to its parent element (the order block)
     // and inside it, look specifically for the <p> that contains the code "VLO-U9BW56".
     // This ensures we are checking the correct code within the correct section.
     // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-U9BW56"]');
     // await expect(orderCode).toBeVisible();
-  
+
     // Alternative using Playwright locators instead of pure XPath:
     // 1) Find the paragraph (or equivalent element) that contains the text "Pedido"
     // 2) Go up to its parent element (the "Pedido" header container)
@@ -36,10 +38,10 @@ test.describe('order lookup', () => {
       .filter({ hasText: /^Pedido$/ })
       .locator('..'); // go up to the parent element of "Pedido"
     await expect(containerPedido).toContainText(testData.orderCode, { timeout: 10_000 });
-  
+
     await expect(page.getByText(testData.orderStatus)).toBeVisible();
   });
-  
+
   test('Should verify an non existing order', async ({ page }) => {
     // testData
     const testData = {
@@ -51,26 +53,32 @@ test.describe('order lookup', () => {
     // Act
     await page.getByLabel('Número do Pedido').fill(testData.orderCode);
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
-  
+
     // Assert
     await expect(page.locator('#root')).toMatchAriaSnapshot(`
       - img
       - heading "Pedido não encontrado" [level=3]
       - paragraph: Verifique o número do pedido e tente novamente`);
-  
+
   });
 
-  test('Should verify an approved with snapshot', async ({ page }) => {
+  test('Should verify an approved order with snapshot', async ({ page }) => {
     // testData
     const testData = {
       orderCode: 'VLO-U9BW56',
       orderStatus: 'APROVADO',
+      carColor: 'Glacier Blue',
+      customerName: 'Yarlei Cruz',
+      customerEmail: 'yarlei@cruz.com.br',
+      customerStore: 'Velô Paulista',
+      customerOrderDate: '01/01/2026',
+      paymentMethod: 'À Vista',
     };
-  
+
     // Act
     await page.getByLabel('Número do Pedido').fill(testData.orderCode);
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
-    
+
     // Assert
     await expect(page.getByTestId(`order-result-${testData.orderCode}`)).toMatchAriaSnapshot(`
       - img
@@ -82,23 +90,89 @@ test.describe('order lookup', () => {
       - paragraph: Modelo
       - paragraph: Velô Sprint
       - paragraph: Cor
-      - paragraph: Glacier Blue
+      - paragraph: ${testData.carColor}
       - paragraph: Interior
       - paragraph: cream
       - paragraph: Rodas
       - paragraph: aero Wheels
       - heading "Dados do Cliente" [level=4]
       - paragraph: Nome
-      - paragraph: Yarlei Cruz
+      - paragraph: ${testData.customerName}
       - paragraph: Email
-      - paragraph: yarlei@cruz.com.br
+      - paragraph: ${testData.customerEmail}
       - paragraph: Loja de Retirada
       - paragraph
       - paragraph: Data do Pedido
       - paragraph: /\\d+\\/\\d+\\/\\d+/
       - heading "Pagamento" [level=4]
-      - paragraph: À Vista
+      - paragraph: ${testData.paymentMethod}
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+  });
+
+  test('Should verify an reproved order with snapshot', async ({ page }) => {
+    // testData
+    const testData = {
+      orderCode: 'VLO-DVVKQC',
+      orderStatus: 'REPROVADO',
+      carColor: 'Midnight Black',
+      customerName: 'Jaci Teixeira',
+      customerEmail: 'jaciteixera@velo.com',
+      customerStore: 'Velô Paulista',
+      customerOrderDate: '01/01/2026',
+      paymentMethod: 'À Vista',
+    };
+
+    // Act
+    await page.getByLabel('Número do Pedido').fill(testData.orderCode);
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    // Assert
+    await expect(page.getByTestId(`order-result-${testData.orderCode}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${testData.orderCode}
+      - img
+      - text: ${testData.orderStatus}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${testData.carColor}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: sport Wheels
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${testData.customerName}
+      - paragraph: Email
+      - paragraph: ${testData.customerEmail}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${testData.paymentMethod}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+  });
+
+  test('Should verify an non existing order with snapshot', async ({ page }) => {
+    // testData
+    const testData = {
+      orderCode: 'VLO-ABC123',
+    };
+
+    // Act
+    await page.getByLabel('Número do Pedido').fill(testData.orderCode);
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    // Assert
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+      - img
+      - heading "Pedido não encontrado" [level=3]
+      - paragraph: Verifique o número do pedido e tente novamente
       `);
   });
 })
